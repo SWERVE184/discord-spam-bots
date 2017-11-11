2a37ebf956d66cbfcfd8c51d87620fe458621666
Liam <Amagicalmailbox@gmail.com>
2017-11-11 15:10:04 -0500

    Trying to fix 'dictionary changed size during iteration'
    "Trying to fix 'dictionary changed size during iteration'"
    
    Should be all good.

--- discord_text_spam_dm.py
+++ discord_text_spam_dm.py
@@ -8,6 +8,7 @@ from config import *
 client = discord.Client()
 token = sys.argv[1]
 spam_text = sys.argv[2]
+UserList = []
 
 @client.event
 async def on_ready():
@@ -16,24 +17,27 @@ async def on_ready():
         if(server.id == DiscordServer):
             for member in server.members:
                 if(member.id != client.user.id):
-                    userNames = open('dm_spam.txt');
-                    text = userNames.read().strip().split()
-                    if member.id in text: 
-                        print(member.name + ' was already messaged')
-                    else:
-                        try:
-                            print('Sent message to '+ member.name)
-                            await client.send_message(member, spam_text)
-                            file = open('dm_spam.txt','a')
-                            file.writelines(member.id + '\n')
-                            file.close()
-                            for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
-                                sys.stdout.write("\r")
-                                sys.stdout.write("{:2d} seconds remaining.".format(remaining))
-                                sys.stdout.flush()
-                                await asyncio.sleep(1)
-                            sys.stdout.write("\rComplete!                    \n")
-                        except Exception:
-                            print('Something went wrong (;3;)') 
+                    UserList.append(member)
+                    
+    for member in UserList:
+        userNames = open('dm_spam.txt');
+        text = userNames.read().strip().split()
+        if member.id in text: 
+            print(member.name + ' was already messaged')
+        else:
+            try:
+                print('Sent message to '+ member.name)
+                await client.send_message(member, spam_text)
+                file = open('dm_spam.txt','a')
+                file.writelines(member.id + '\n')
+                file.close()
+                for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
+                    sys.stdout.write("\r")
+                    sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                    sys.stdout.flush()
+                    await asyncio.sleep(1)
+                sys.stdout.write("\rComplete!                    \n")
+            except Exception:
+                print('Something went wrong (;3;)') 
     
 client.run(token, bot=False)
