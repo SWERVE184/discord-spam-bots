5e2651f34bb0799f949ad4333490669889096667
Liam <Amagicalmailbox@gmail.com>
2017-11-11 19:32:53 -0500

    Added Support For Scanning All Joined Servers.
    Change 'ScanAllServers' to True for this to work.

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ userToken = [''] # User Token here, multiple accounts may work. example 'token',
 DiscordChannel = '' # Channel ID,
 
 #DM Spam
-DiscordServer = '' #Server ID (https://a.safe.moe/KP4DR.png this one)
+DiscordServer = '' # Server ID (https://a.safe.moe/KP4DR.png this one)
+ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
