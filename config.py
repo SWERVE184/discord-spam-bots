cd9abba791a2154143f32f3e757e5559643372b1
Liam <Amagicalmailbox@gmail.com>
2017-12-13 20:38:13 -0500

    Added Spam Speed to config.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Basic Config (For all bots)
 userToken = [''] # User Token here, multiple accounts may work. example 'token','token'
 DiscordChannel = '' # Channel ID,
-SpamSpeed = '0.7' # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
+SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 
 #DM Spam
 DiscordServer = '' # Server ID (https://a.safe.moe/KP4DR.png this one)
