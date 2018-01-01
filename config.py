7ad0b003c63812abc4ef9415a1647d7c09b84a4c
Liam <Amagicalmailbox@gmail.com>
2018-01-01 12:52:00 -0500

    Added Heavy Scrape to config
    An attempt to use the bots with large servers.

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 
 #DM Spam
 DiscordServer = '' # Server ID (https://a.safe.moe/KP4DR.png this one)
 ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
+HeavyScrape = False # Use this for servers with a lot of users
+
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
