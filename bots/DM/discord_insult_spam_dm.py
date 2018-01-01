a4593555e2125e6726a1c52c5ee117f5451b2164
Liam <Amagicalmailbox@gmail.com>
2018-01-01 12:52:45 -0500

    Added Heavy Scrape

--- bots/DM/discord_insult_spam_dm.py
+++ bots/DM/discord_insult_spam_dm.py
@@ -17,12 +17,12 @@ async def on_ready():
     for server in client.servers:
         if ScanAllServers == False:
             if(server.id == DiscordServer):
-                UserList = server.members
+                UserList = list(server.members)
         else:
-            UserList = server.members
-
-    for member in list(UserList):
-        if(member.id != client.user.id):
+            UserList = list(server.members)
+            
+    if HeavyScrape == False:  
+        for member in UserList:
             userNames = open('dm_spam_insult.txt');
             text = userNames.read().strip().split()
             if member.id in text: 
@@ -45,6 +45,33 @@ async def on_ready():
                         await asyncio.sleep(1)
                     sys.stdout.write("\rComplete!                    \n")
                 except Exception:
-                    print('Something went wrong (;3;) relaunching...') 
+                    print('Something went wrong (;3;) relaunching...')
+    else:
+        while not client.is_closed:
+            member = random.choice(UserList)
+            if(member.id != client.user.id):
+            userNames = open('dm_spam_insult.txt');
+            text = userNames.read().strip().split()
+            if member.id in text: 
+                print(member.name + ' was already messaged')
+            else:
+                try:
+                    print('Sent message to '+ member.name)
+                    file = open('dm_spam_insult.txt','a')
+                    file.writelines(member.id + '\n')
+                    file.close()
+                    html = urllib.request.urlopen("https://insult.mattbas.org/api/insult.html").read()
+                    soup = BeautifulSoup(html,"html.parser")
+                    insult_text = soup.find('h1')
+                    print(insult_text.text)
+                    await client.send_message(member, insult_text.text)
+                    for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
+                        sys.stdout.write("\r")
+                        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                        sys.stdout.flush()
+                        await asyncio.sleep(1)
+                    sys.stdout.write("\rComplete!                    \n")
+                except Exception:
+                    print('Something went wrong (;3;) relaunching...')
     
 client.run(token,bot=False)
--- bots/DM/discord_text_spam_dm.py
+++ bots/DM/discord_text_spam_dm.py
@@ -19,30 +19,53 @@ async def on_ready():
     for server in client.servers:
         if ScanAllServers == False:
             if(server.id == DiscordServer):
-                UserList = server.members
+                UserList = list(server.members)
         else:
-            UserList = server.members
-                    
-    for member in list(UserList):
-        if(member.id != client.user.id):
-            userNames = open('dm_spam_text.txt');
-            text = userNames.read().strip().split()
-            if member.id in text: 
-                print(member.name + ' was already messaged')
-            else:
-                try:
-                    await client.send_message(member,spam_text+" "+random.choice(emojiList)+random.choice(emojiList))
-                    print('Sent message to '+ member.name)
-                    file = open('dm_spam_text.txt','a')
-                    file.writelines(member.id + '\n')
-                    file.close()
-                    for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
-                        sys.stdout.write("\r")
-                        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
-                        sys.stdout.flush()
-                        await asyncio.sleep(1)
-                    sys.stdout.write("\rComplete!                    \n")
-                except Exception:
-                    print('Something went wrong (;3;) relaunching...') 
+            UserList = list(server.members)
+    if HeavyScrape == False:                
+        for member in UserList:
+            if(member.id != client.user.id):
+                userNames = open('dm_spam_text.txt');
+                text = userNames.read().strip().split()
+                if member.id in text: 
+                    print(member.name + ' was already messaged')
+                else:
+                    try:
+                        await client.send_message(member,spam_text+" "+random.choice(emojiList)+random.choice(emojiList))
+                        print('Sent message to '+ member.name)
+                        file = open('dm_spam_text.txt','a')
+                        file.writelines(member.id + '\n')
+                        file.close()
+                        for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
+                            sys.stdout.write("\r")
+                            sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                            sys.stdout.flush()
+                            await asyncio.sleep(1)
+                        sys.stdout.write("\rComplete!                    \n")
+                    except Exception:
+                        print('Something went wrong (;3;) relaunching...') 
+    else:
+        while not client.is_closed:
+            member = random.choice(UserList)
+            if(member.id != client.user.id):
+                userNames = open('dm_spam_text.txt');
+                text = userNames.read().strip().split()
+                if member.id in text: 
+                    print(member.name + ' was already messaged')
+                else:
+                    try:
+                        await client.send_message(member,spam_text+" "+random.choice(emojiList)+random.choice(emojiList))
+                        print('Sent message to '+ member.name)
+                        file = open('dm_spam_text.txt','a')
+                        file.writelines(member.id + '\n')
+                        file.close()
+                        for remaining in range(31, 0, -1):# Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
+                            sys.stdout.write("\r")
+                            sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                            sys.stdout.flush()
+                            await asyncio.sleep(1)
+                        sys.stdout.write("\rComplete!                    \n")
+                    except Exception:
+                        print('Something went wrong (;3;) relaunching...')             
 
 client.run(token, bot=False)
